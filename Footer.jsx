de504f689722f7410edb001d4fcef336c37d29a2
abinaya003 <84920593+abinaya003@users.noreply.github.com>
2021-06-27 19:26:28 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer><p>Copyrigth By ShapeAI @ {new Date().getFullYear()}</p></footer>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A Basic Web Development React Js Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
